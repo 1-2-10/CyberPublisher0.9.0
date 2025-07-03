aa146450fa47ff37f40a1664060ce53866ff5b19
1-2-10 <48568372+1-2-10@users.noreply.github.com>
2025-07-03 13:26:29 -0400

    Create py_slices.py

---
+++ py_slices.py
@@ -0,0 +1,4 @@
+from py_slices.file_picker import pick_template, pick_data_xml, pick_output_location
+from py_slices.config_manager import write_conf, last_check
+from py_slices.publisher import publish
+from py_slices.reset import reset_all
